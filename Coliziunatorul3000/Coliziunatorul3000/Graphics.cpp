3b7faa53e22942acf59cb600bab2802126e07056
tibu89 <andrei.tibuleac89@gmail.com>
2014-12-07 23:14:30 +0200

    Added GL stuff

---
+++ Coliziunatorul3000/Coliziunatorul3000/Graphics.cpp
@@ -0,0 +1,6 @@
+#include "Graphics.h"
+
+void Graphics::Init(int w, int h, std::string title)
+{
+	
+}
---
+++ Coliziunatorul3000/Coliziunatorul3000/Graphics.h
@@ -0,0 +1,10 @@
+#include<GL\glew\glew.h>
+#include<GL\GLFW\glfw3.h>
+
+#include<string>
+
+class Graphics
+{
+public:
+	void Init(int, int, std::string);
+};
