ba2b269821957d9bebc5083e7cc2094724a084f4
tibu89 <andrei.tibuleac89@gmail.com>
2014-12-13 18:14:01 +0200

    Working camera

--- Coliziunatorul3000/Coliziunatorul3000/ThirdPersonCamera.cpp
+++ Coliziunatorul3000/Coliziunatorul3000/ThirdPersonCamera.cpp
@@ -5,7 +5,39 @@
 glm::mat4 ThirdPersonCamera::GetViewMatrix()
 {
 	glm::vec3 direction = glm::vec3(cos(pitch) * sin(yaw), sin(pitch), cos(pitch) * cos(yaw));
-	glm::vec3 right = glm::vec3(sin(yaw - glm::pi<float>()), 0.f, cos(yaw - glm::pi<float>()));
+	glm::vec3 right = glm::vec3(sin(yaw - glm::half_pi<float>()), 0.f, cos(yaw - glm::half_pi<float>()));
 	glm::vec3 up = glm::cross(right, direction);
-	return glm::lookAt(target + direction * zoom, target, up);
+	return glm::lookAt(target - direction * zoom, target, up);
+}
+
+void ThirdPersonCamera::Translate(glm::vec3 offset)
+{
+	target += offset;
+}
+	
+void ThirdPersonCamera::RotatePitch(float rads)
+{
+	pitch += rads;
+	if(pitch >= glm::half_pi<float>())
+	{
+		pitch = glm::half_pi<float>();
+	}
+	else if(pitch <= -glm::half_pi<float>())
+	{
+		pitch = -glm::half_pi<float>();
+	}
+}
+
+void ThirdPersonCamera::RotateYaw(float rads)
+{
+	yaw += rads;
+
+	if(yaw >= 2 * glm::pi<float>())
+	{
+		yaw -= 2 * glm::pi<float>();
+	}
+	else if(yaw < 0)
+	{
+		yaw += 2 * glm::pi<float>();
+	}
 }
--- Coliziunatorul3000/Coliziunatorul3000/main.cpp
+++ Coliziunatorul3000/Coliziunatorul3000/main.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
+#include <GL\glm\gtc\matrix_transform.hpp>
+#include <GL\glm\gtx\transform.hpp>
 
 #include "Graphics.h"
+#include "ThirdPersonCamera.h"
+
+#define MOUSE_SPEED (0.01f)
 
 Graphics* graphics = NULL;
+
+void CheckCameraInputs(GLFWwindow* window, Camera* camera)
+{
+	int halfWidth, halfHeight;
+    glfwGetFramebufferSize(window, &halfWidth, &halfHeight);
+	halfWidth /= 2;
+	halfHeight /= 2;
+
+	double xpos = 0, ypos = 0;
+
+	glfwGetCursorPos(window, &xpos, &ypos);
+
+	camera->RotateYaw  ((xpos - halfWidth)  * MOUSE_SPEED);
+	camera->RotatePitch((ypos - halfHeight) * MOUSE_SPEED);
+
+	glfwSetCursorPos(window, halfWidth, halfHeight);
+}
 
 int main()
 {
 	graphics = new Graphics();
 	graphics->Init(640, 480, "Coliziunatorul3000");
+	
+    int width, height;
+    glfwGetFramebufferSize(graphics->GetWindow(), &width, &height);
+
+	glfwSetInputMode(graphics->GetWindow(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+	glfwSetCursorPos(graphics->GetWindow(), width / 2, height / 2);
+
+	Camera* camera = new ThirdPersonCamera(0.f, glm::pi<float>(), glm::zero<glm::vec3>());
+
+	glm::mat4 perspectiveMatrix = glm::perspective(45.f, 4.f / 3.f, 1.0f, 200.0f);
+	glm::mat4 viewMatrix;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(&perspectiveMatrix[0][0]);
 
 	while(!glfwWindowShouldClose(graphics->GetWindow()))
 	{
+		CheckCameraInputs(graphics->GetWindow(), camera);
+
         float ratio;
-        int width, height;
         glfwGetFramebufferSize(graphics->GetWindow(), &width, &height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		viewMatrix = camera->GetViewMatrix();
+		glLoadMatrixf(&viewMatrix[0][0]);
         glBegin(GL_TRIANGLES);
         glColor3f(1.f, 0.f, 0.f);
         glVertex3f(-0.6f, -0.4f, 0.f);
